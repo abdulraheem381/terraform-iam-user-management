6bb4a5a12d9018589e93323e1ae4c1a074f8009b
abdulraheem381 <shadowpulse.pentest@gmail.com>
2025-09-09 11:41:51 +0500

    Project Done

--- providers.tf
+++ providers.tf
@@ -1,3 +1,15 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.11.0"
+    }
+  }
+
+}
+
+provider "aws" {
+
+  region = "ap-south-1"
 
 }
--- users.tf
+++ users.tf
@@ -1,9 +1,32 @@
 locals {
-  users_from_yml = yamldecode(file("${path.module}/user-roles.yml")).users
+  users_from_yaml = yamldecode(file("${path.module}/user-roles.yaml")).users
+  users_map = {
+    for user_config in local.users_from_yaml : user_config.username => user_config.roles
+  }
 }
 
-output "users" {
+resource "aws_iam_user" "users" {
+  for_each = toset(local.users_from_yaml[*].username)
+  name     = each.value
+}
 
-  value = local.users_from_yml
+resource "aws_iam_user_login_profile" "users" {
+  for_each        = aws_iam_user.users
+  user            = each.value.name
+  password_length = 8
 
+  lifecycle {
+    ignore_changes = [
+      password_length,
+      password_reset_required,
+      pgp_key
+    ]
+  }
+}
+
+output "passwords" {
+  sensitive = true
+  value = {
+    for user, user_login in aws_iam_user_login_profile.users : user => user_login.password
+  }
 }
